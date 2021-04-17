813b9eb1543706a4b6433201e05cb59d9d3638e5
Alessandro Versace <aleversace98@gmail.com>
2021-04-17 12:21:38 +0200

    feats: added Naive Bayes and Tied Cov Classifiers to iris dataset

--- lab/lab5_Generative_Models_1/generative_models_1.py
+++ lab/lab5_Generative_Models_1/generative_models_1.py
@@ -42,9 +42,21 @@ def logMVG(x, mu, C):
     return - x.shape[0]/2 * np.log(2*np.pi) - 1/2 * (np.linalg.slogdet(C)[1]) - 1/2*((np.dot((x - mu).T, np.linalg.inv(C))).T * (x - mu)).sum(axis=0)
 
 
-if __name__=='__main__':
-    D, L = load_iris()
-    (DTR, LTR), (DTE, LTE) = split_db_2to1(D, L)
+# Uses log-MVG probability process to estimate labels and prints results
+def computeAndStats(ld1, ld2, ld3):
+    lS = np.vstack((ld1, ld2, ld3))
+    LSjoint = lS + np.log(1/3)
+    LMarginalP = scipy.special.logsumexp(LSjoint, axis=0)
+    logSPost = LSjoint - LMarginalP
+    lPredicted = logSPost.argmax(axis=0)
+    print("\nTied Covariance Gaussian Classifier:\nPredicted Classes:\n{}".format(lPredicted))
+    lacc = ((lPredicted == LTE).sum()) / (LTE.shape[0])
+    lerr = 1 - lacc
+    print("\nAccuracy: {}, Error: {}".format(lacc, lerr))
+
+
+# Function that uses a MVG and a log-MVG approach
+def GaussianClassifiers(DTR, LTR, DTE, LTE, D, L):
 
     DTR1 = DTR[:, (LTR == L[0])]
     DTR2 = DTR[:, (LTR == L[50])]
@@ -75,7 +87,7 @@ if __name__=='__main__':
     SPost = Sjoint / MarginalP
     # Predicted Label
     PredictedClass = SPost.argmax(axis=0)
-    print("\nPredicted classes: {}".format(PredictedClass))
+    print("\nMVG Classifier:\nPredicted classes: {}".format(PredictedClass))
 
     ##
     # Model Accuracy (Number of correct estimations on the evaluation set)
@@ -85,15 +97,14 @@ if __name__=='__main__':
     print("\nAccuracy: {}, Error: {}".format(acc, err))
 
 
-    ###
-    # Working with log densities is less problematic (numerical issues) and we obtain the same result
-    ###
-    print("\nUsing log densities:")
+    #####
+    ## Working with log-MVG densities is less problematic (numerical issues) and we obtain the same result
+    #####
 
     ld1 = logMVG(DTE, MUc1, Cc1)
     ld2 = logMVG(DTE, MUc2, Cc2)
     ld3 = logMVG(DTE, MUc3, Cc3)
-    lS = np.vstack((d1, d2, d3))
+    lS = np.vstack((ld1, ld2, ld3))
     LSjoint = lS + np.log(1/3)
 
     LMarginalP = scipy.special.logsumexp(LSjoint, axis=0)     # More robust
@@ -103,10 +114,38 @@ if __name__=='__main__':
     logSPost = LSjoint - LMarginalP
 
     lPredicted = logSPost.argmax(axis=0)
-    print(lPredicted)
+    print("\nlog-MVG Classifier:\nPredicted Classes:\n{}".format(lPredicted))
     lacc = ((lPredicted == LTE).sum()) / (LTE.shape[0])
     lerr = 1 - lacc
     print("\nAccuracy: {}, Error: {}".format(lacc, lerr))
 
 
+    #####
+    ## Naive Bayes Gaussian Classifier (deducted from MVG)
+    #####
 
+    NBCov1 = Cc1*np.eye(Cc1.shape[0])
+    NBCov2 = Cc2*np.eye(Cc2.shape[0])
+    NBCov3 = Cc3*np.eye(Cc3.shape[0])
+    ld1 = logMVG(DTE, MUc1, NBCov1)
+    ld2 = logMVG(DTE, MUc2, NBCov2)
+    ld3 = logMVG(DTE, MUc3, NBCov3)
+    computeAndStats(ld1, ld2, ld3)
+
+
+
+    #####
+    ## Tied Covariance Gaussian Classifier (deducted from MVG)
+    #####
+    WithinCov = 1/DTR.shape[1] * (DTR1.shape[1]*Cc1 + DTR2.shape[1]*Cc2 + DTR3.shape[1]*Cc3)   # Within Class cov matrix deducted from MVG (Same of LDA)
+    ld1 = logMVG(DTE, MUc1, WithinCov)
+    ld2 = logMVG(DTE, MUc2, WithinCov)
+    ld3 = logMVG(DTE, MUc3, WithinCov)
+    computeAndStats(ld1, ld2, ld3)
+
+
+
+if __name__=='__main__':
+    D, L = load_iris()
+    (DTR, LTR), (DTE, LTE) = split_db_2to1(D, L)
+    GaussianClassifiers(DTR, LTR, DTE, LTE, D, L)
